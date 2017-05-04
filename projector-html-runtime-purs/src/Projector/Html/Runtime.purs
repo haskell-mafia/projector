3792b81234abd734b34d7afc8a84b016be18c6fe
Tim Humphries <tim.humphries@ambiata.com>
2017-05-04 17:01:06 +1000

    Update Purescript runtime somewhat

--- projector-html-runtime-purs/src/Projector/Html/Runtime.purs
+++ projector-html-runtime-purs/src/Projector/Html/Runtime.purs
@@ -1,8 +1,11 @@
 module Projector.Html.Runtime (
     append
   , concat
+  , fold
+  , isEmpty
   ) where
 
+import Control.Monad as Monad
 import Data.String as String
 import Prelude ((<>))
 
@@ -13,3 +16,11 @@ append =
 concat :: Array String -> String
 concat =
   String.joinWith ""
+
+fold :: forall a. Array (Array a) -> Array a
+fold =
+  Monad.join
+
+isEmpty :: forall a. Array a -> Boolean
+isEmpty =
+  Array.null
