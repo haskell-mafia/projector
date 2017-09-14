166cbef811786fe791773a2e8a367c48e6c19541
Tim Humphries <tim.humphries@ambiata.com>
2017-09-14 16:49:55 +1000

    fixup! Attempt to simplify further

--- projector-html-purs/src/Projector/Html/Backend/Purescript.hs
+++ projector-html-purs/src/Projector/Html/Backend/Purescript.hs
@@ -264,7 +264,7 @@ genLit :: Value PurescriptPrimT -> Doc a
 genLit v =
   case v of
     PTextV x ->
-      show x
+      WL.string (show x)
 
 -- -----------------------------------------------------------------------------
 
